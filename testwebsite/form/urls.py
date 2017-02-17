bf4a4572a755de9cfcdaadc1a4a2091a589761d1
Hussam Butt <hussambutt.alayacare@gmail.com>
2017-02-16 20:16:13 -0500

    Add url and first template for site

--- testwebsite/form/urls.py
+++ testwebsite/form/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    url(r'info/', views.detail, name='detail'),
 ]
--- testwebsite/form/views.py
+++ testwebsite/form/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("<h1>Still working on it</h1>")
+
+def detail(request):
+    template = loader.get_template('form/index.html')
+    return HttpResponse(template.render(request))
